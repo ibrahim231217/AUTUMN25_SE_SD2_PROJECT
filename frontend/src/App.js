87d45a12f68f520035158bddcaa95a4712242a6c
ibrahim231217 <139586386+ibrahim231217@users.noreply.github.com>
2025-11-13 14:50:17 +0600

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hospital Management System Sign UP and Log In </h1>
+    </div>
+  );
+}
+
+export default App;
